a0a564a7264f743a5109b4bc8fd1a56f5ca66ee9
Leon Zuev <loonatune@gmail.com>
2023-03-17 21:31:19 +0200

    Add Game and bodies of classes

---
+++ src/Room.java
@@ -0,0 +1,12 @@
+public class Room {
+  String title;
+
+  public  Room(String title) {
+    this.title = title;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+}
---
+++ src/quest/Game.java
@@ -0,0 +1,23 @@
+package quest;
+
+import java.util.List;
+
+public class Game {
+
+  private Hero mainHero;
+  private List<Room> rooms;
+
+  public Game() {
+    // здесь будут комнаты
+  }
+
+  public void start() {
+    // здесь будет герой
+    mainCycle();//запускаем основной игровой цикл
+
+  }
+
+  public void mainCycle() {
+    // основной игровой цикл
+  }
+}
